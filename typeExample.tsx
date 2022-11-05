f4e22e864045929187c875b2566ab1632d66b969
Eric Navar <eric.navar@asurion.com>
2022-11-05 19:06:36 -0400

    first commit

---
+++ typeExample.tsx
@@ -0,0 +1,13 @@
+type Profile = {
+    name: string;
+    age: number;
+    phone: string | number;
+    address?: string;
+}
+
+const saul: Profile = {
+    name: 'Saul Goodman',
+    age: 41,
+    phone: '(505) 503-4455',
+    address: '9800 Montgomery Blvd NE, Albuquerque',
+};
---
+++ typeExample2.tsx
@@ -0,0 +1,23 @@
+type Address = {
+    line1: string;
+    city: string;
+    state: string;
+}
+
+type Profile2 = {
+    name: string;
+    age: number;
+    phone: string | number;
+    address: Address;
+}
+
+const saul2: Profile2 = {
+    name: 'Saul Goodman',
+    age: 41,
+    phone: '(505) 503-4455',
+    address: {
+        line1: '9800 Montgomery Blvd NE',
+        city: 'Albuquerque',
+        state: 'New Mexico',
+    }
+};
